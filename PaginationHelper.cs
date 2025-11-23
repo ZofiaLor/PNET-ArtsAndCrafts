7791fd243b4aabbce8e34adbf7435f26364b7821
ZofiaLor <zosia984@gmail.com>
2025-11-23 14:27:48 +0100

    Move comment logic to a reusable component

---
+++ PaginationHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace ArtsAndCrafts
+{
+    public static class PaginationHelper
+    {
+        // https://stackoverflow.com/questions/6185159/linq-and-pagination
+        public static IEnumerable<T> Page<T>(this IEnumerable<T> list, int page, int pageSize)
+        {
+            return list.Skip((page -  1) * pageSize).Take(pageSize);
+        }
+    }
+}
